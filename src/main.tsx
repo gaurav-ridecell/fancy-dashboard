2e7439b8ed823e65d3d8cc2a58c4056d66500709
Gaurav Burjwal <gaurav.burjwal@ridecell.com>
2025-05-20 12:03:18 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,27 @@
-
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 // Define the render function that takes elementId and token as parameters
 const renderMyApp = (elementId: string, token: string) => {
   const container = document.getElementById(elementId);
-  
+
   if (!container) {
     console.error(`Failed to find the element with id: ${elementId}`);
     return;
   }
-  
+
   const root = createRoot(container);
   root.render(
     <StrictMode>
       <App token={token} />
-    </StrictMode>
+    </StrictMode>,
   );
 };
 
 // Define the specific key for the window object
-const KEY = 'a-074347';
+const KEY = "fancy-dashboard";
 
 // Expose the render function to the window object
 window[KEY] = renderMyApp;
-
